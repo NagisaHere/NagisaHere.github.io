05a678a03cdd525d506a256705269b4073cb6e50
NagisaHere <rw75000777@gmail.com>
2024-07-08 09:55:14 +1000

    added small navbar line thing

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+
+    /** 
+    Comments cant be written inside returns (well shocker)
+
+    the wacky div with py-4 border-b-[1px] I have no idea why its formatted
+    that way but it is what it is?
+    */
+    return (
+        <div className="fixed w-full text-white bg-black z-10 shadow-sm">
+            
+            <div
+              className="
+                py-4
+                border-b-[1px]
+              "
+            >
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 export default function Home() {
   return (
-    <div>Hello</div>
+    // applying css seems to be through classNames
+    <div className="text-rose-500 text-2xl">Hello</div>
   );
 }
