476b9c6921793289a0acaba1d591e36c634a1e3c
NagisaHere <rw75000777@gmail.com>
2025-01-21 22:45:52 +1000

    starting threejs component

---
+++ app/components/Background.tsx
@@ -0,0 +1,10 @@
+const Background = () => {
+    return (
+        <canvas className=" fixed top-0 left-0"
+            id="bg">
+                
+        </canvas>
+    );
+}
+
+export default Background;
