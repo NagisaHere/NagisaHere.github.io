f198e96edad6c04ccacb5f3810a1589cc590b510
Ryan Wang <s4884364@student.uq.edu.au>
2026-03-19 20:31:22 +1000

    yeah I have no idea what I amdoing

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 export default defineConfig({
   
-  server: {
-    open: '/experiment.html'
-  },
-    build: {
-    rollupOptions: {
-      input: {
-        // 'main' is just an internal name for the chunk. 
-        // Replace 'experiment.html' with your actual file name.
-        main: resolve(__dirname, 'experiment.html')
-      }
-    }
-  } 
+
 });
